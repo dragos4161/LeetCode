be8c5a7ce6fb8198a11b01afff233f449d55e51f
DragosDima0 <76730593+DragosDima0@users.noreply.github.com>
2022-06-10 15:49:45 +0300

    Happy Number

--- HappyNumber.py
+++ HappyNumber.py
@@ -1,23 +1,21 @@
-class Solution(object):
-    def isHappy(self, n):
+def isHappy(n):
+    suma = 0
+    seen = {}
+    nr = n
+    while nr != 1:
         suma = 0
-        seen = {}
-        nr = n
-        while nr != 1:
-            suma = 0
-            while n != 0:
-                suma += (n % 10) ** 2
-                n /= 10
-            nr = suma
-            n = suma
-            if suma not in seen:
-                seen[suma] = 1
-            else:
-                return False
-        return True
+        while n != 0:
+            suma += (n % 10) ** 2
+            n /= 10
+        nr = suma
+        n = suma
+        if suma not in seen:
+            seen[suma] = 1
+        else:
+            return False
+    return True
 
-sol = Solution()
-if sol.isHappy(1) == True:
+if isHappy(19) == True:
     print("Happy number")
 else:
     print("Not so happy number")
