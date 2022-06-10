1a2bd77d6c3aa388d1824f6f11d22924a32f825b
DragosDima0 <76730593+DragosDima0@users.noreply.github.com>
2022-06-10 15:01:11 +0300

    semn revizuit

--- SignOfTheProductOfanArray.py
+++ SignOfTheProductOfanArray.py
@@ -1,9 +1,8 @@
-class Solution(object):
-    def arraySign(self, nums):
-        sign = 1
-        for n in nums:
-            if n == 0:
-                return 0
-            if n < 0:
-                sign = -sign
-        return sign
+def arraySign(self, nums):
+    sign = 1
+    for n in nums:
+        if n == 0:
+            return 0
+        if n < 0:
+            sign = -sign
+    return sign
